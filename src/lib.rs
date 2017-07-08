6236280a21a7565b71c9cb17e01f7a2d394089e6
Joaquín R <globojorro@gmail.com>
2017-07-08 14:43:28 -0500

    basic documentation and setup of error-chain

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Shared code for EZO sensor chips. These chips are used for sensing aquatic
+//! media.
+
+#![recursion_limit = "1024"]
+#[macro_use]
+extern crate error_chain;
+
+/// Use error-chain for error-handling.
+pub mod errors {
+    error_chain!{}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
