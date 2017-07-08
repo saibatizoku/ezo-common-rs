a5fa320ed03e36a986669a04a76d4ad2d8ee6280
Joaquín R <globojorro@gmail.com>
2017-07-08 14:57:10 -0500

    add allowable baudrates

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,19 @@ pub mod errors {
     error_chain!{}
 }
 
+/// Allowable baudrates used when changing the chip to UART mode.
+#[derive(Debug)]
+pub enum BpsRate {
+    Bps300 = 300,
+    Bps1200 = 1200,
+    Bps2400 = 2400,
+    Bps9600 = 9600,
+    Bps19200 = 19200,
+    Bps38400 = 38400,
+    Bps57600 = 57600,
+    Bps115200 = 115200,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
